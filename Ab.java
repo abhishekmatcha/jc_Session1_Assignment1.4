fbe56c528b3fb9c862d7a427077b5d2cb39f93f8
abhishekmatcha <seshaabhishek@gmail.com>
2017-01-30 00:37:13 +0530

    Add files via upload

---
+++ Ab.java
@@ -0,0 +1,13 @@
+class Ab
+{
+	public static void main(String[] args)
+	{
+		int a=65;
+		for(int i=a;i<=90;i++)
+		{
+			String ch=new Character((char)i).toString();
+			System.out.println(ch);
+		}
+			
+	}
+}
